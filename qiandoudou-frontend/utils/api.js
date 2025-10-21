6ecce9182d777a7703f67eb71a09aa4eb0ef471b
Solomon258 <1120875952@qq.com>
2025-10-21 19:12:51 +0800

    梦想攒2

--- qiandoudou-frontend/utils/api.js
+++ qiandoudou-frontend/utils/api.js
@@ -2,8 +2,8 @@
 const app = getApp()
 
 // 后端API基础地址
-const BASE_URL = 'http://localhost:8080/api'  // 本地开发
-//  const BASE_URL = 'https://heartllo.cn/api'  // 生产环境域名
+// const BASE_URL = 'http://localhost:8080/api'  // 本地开发
+ const BASE_URL = 'https://heartllo.cn/api'  // 生产环境域名
 
 
 /**
